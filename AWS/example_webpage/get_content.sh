d145944ca3240e961ed2f0fa7c1bdf61650f1223
Jeremy McCormick <jermccormick@gmail.com>
2019-05-31 18:16:58 +0700

    Remove script.

--- AWS/example_webpage/get_content.sh
+++
@@ -1,2 +0,0 @@
-wget https://raw.githubusercontent.com/spuriq/Summer2019/17954f7675d1cc66a7505f68b2921547f2f4c0fe/AWS/example_webpage/index.html
-wget https://raw.githubusercontent.com/spuriq/Summer2019/17954f7675d1cc66a7505f68b2921547f2f4c0fe/AWS/example_webpage/spuriq_logo.png 
